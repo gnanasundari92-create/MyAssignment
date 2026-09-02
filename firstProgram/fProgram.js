8c4c9ded10421f931ed41d03fcdf55279ba4c3a5
gnanasundari92-create <gnanasundari.92@gmail.com>
2026-09-02 18:41:54 +0530

    Add files via upload

---
+++ firstProgram/fProgram.js
@@ -0,0 +1,22 @@
+var firstName = "Gnana Sundari P"
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName = "Testleaf"
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber = 9999222212
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+
+
