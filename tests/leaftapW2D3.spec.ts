5d0024125c5689b28f2f5dac49c1e543dc137a66
gnanasundari92-create <gnanasundari.92@gmail.com>
2026-09-15 02:27:59 +0530

    Add files via upload

--- tests/leaftapW2D3.spec.ts
+++ tests/leaftapW2D3.spec.ts
@@ -4,15 +4,24 @@ test('learn to launch the browser',async() => {
     const browser= await chromium.launch({headless:false})
     const context=await browser.newContext()
     const page=await context.newPage()
-    
+
+    //Logging into leaftaps with provided credentials
     await page.goto("http://leaftaps.com/opentaps/control/main");
     await page.locator('#username').fill('democsr2');  
     await page.locator('#password').fill('crmsfa');
-    await page.locator('.decorativeSubmit').click();   
+    await page.locator('.decorativeSubmit').click();  
+
+    //clicking on CRM/SFA link
     await page.getByText('CRM/SFA ').click();
+    
+    //Clicking on Leads tab
     await page.locator('a[href="/crmsfa/control/leadsMain"]').click();
     page.waitForTimeout(5000)
+
+    //Clicking on Create Lead
     await page.locator('a[href="/crmsfa/control/createLeadForm"]').click();
+
+    //Entering the value for Comoany name, First name, Last name, Department, Title and Annual Revenue
     await page.locator('input[name="companyName"]').fill("Test Leaf");
     await page.locator('#createLeadForm_firstName').fill('Gnana Sundari');
     await page.locator('#createLeadForm_lastName').fill('Parama Sivam');
@@ -21,13 +30,12 @@ test('learn to launch the browser',async() => {
     await page.locator('#createLeadForm_departmentName').fill("QA");
     await page.locator('#createLeadForm_annualRevenue').fill('120000');
    
+    //Printing the dropdown values of Source field
     const optionsText=await page.locator('#createLeadForm_dataSourceId>option').allInnerTexts();
     console.log(optionsText);
-    console.log(optionsText.length);
     
-    
+    //Entering phone number and clicking on Create Lead 
     await page.locator('#createLeadForm_primaryPhoneNumber').fill('9876543210');
     await page.locator('.smallSubmit').click();
-
 }
 )
